4bef165e2a5e96bed2fe8dd308639e5c8fb4204a
Yu Changyuan <reivzy@gmail.com>
2025-03-16 13:32:17 +0800

    frame: add Error for frame mod

--- src/frame.rs
+++ src/frame.rs
@@ -1,20 +1,31 @@
 use embedded_can::{Frame, Id};
 use serde::{Deserialize, Serialize};
 
-use crate::{from_bytes, to_bytes, Result, Error};
+use crate::{from_bytes, to_bytes};
 
-pub fn from_frame<'a, T: Deserialize<'a>, F: Frame>(f: &'a F) -> Result<T> {
-    if f.is_remote_frame() {
-        return Err(Error::Other("remote frame"))
-    }
-
-    from_bytes::<T>(f.data())
+#[derive(Debug, thiserror_no_std::Error, PartialEq)]
+pub enum Error {
+    #[error("remote frame")]
+    RemoteFrame,
+    #[error("message too long")]
+    MsgTooLong,
+    #[error("serde error {0}")]
+    SerdeErr(crate::Error),
 }
 
-pub fn to_frame<T: Serialize, F: Frame, I: Into<Id>>(id: I, a: &T) -> Result<F> {
-    let data = to_bytes::<T>(a)?;
+pub fn from_frame<'a, T: Deserialize<'a>, F: Frame>(f: &'a F) -> Result<T, Error> {
+    if f.is_remote_frame() {
+        return Err(Error::RemoteFrame)
+    }
+
+    from_bytes::<T>(f.data()).map_err(Error::SerdeErr)
+}
+
+pub fn to_frame<T: Serialize, F: Frame, I: Into<Id>>(id: I, a: &T) -> Result<F, Error> {
+    let data = to_bytes::<T>(a).map_err(Error::SerdeErr)?;
+
     match F::new(id, data.as_slice()) {
         Some(f) => Ok(f),
-        None => Err(Error::SerMsgTooLong)
+        None => Err(Error::MsgTooLong)
     }
 }
